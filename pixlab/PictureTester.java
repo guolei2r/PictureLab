0c03894abae40283e6ef654ac21dd00d19e46fa2
guolei2r <43968501+guolei2r@users.noreply.github.com>
2019-03-11 11:49:36 -0700

    ok
    ok

--- pixlab/PictureTester.java
+++ pixlab/PictureTester.java
@@ -26,6 +26,14 @@ public class PictureTester
     caterpillar.explore();
   }
   
+   public static void testMirrorVerticalRightToLeft()
+  {
+    Picture caterpillar = new Picture("caterpillar.jpg");
+    caterpillar.explore();
+    caterpillar.mirrorVerticalRightToLeft();
+    caterpillar.explore();
+  }
+  
   /** Method to test mirrorTemple */
   public static void testMirrorTemple()
   {
